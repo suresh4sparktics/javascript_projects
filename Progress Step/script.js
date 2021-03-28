dea3ea9fb5a09d20a17c4f2680c3fa028acd0845
suresh4sparktics <suresh.p@sparktics.com>
2021-03-28 13:41:11 +0530

    Added Progress Bar Functionality

---
+++ Progress Step/script.js
@@ -0,0 +1,48 @@
+// window.onload = () => {
+let currentActive = 1;
+var progress = document.getElementById("progress");
+var circles = document.querySelectorAll(".circle");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+// console.log({ progress, circles, prev, next });
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive == 1) {
+    prev.disabled = true;
+  } else if (currentActive == circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
+
+// };
